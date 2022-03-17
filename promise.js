05d571ffd88f8afedfa6958d1ae2de1df11781b4
zhangpengwang <zhangpengwang@s-hhc.com>
2022-03-17 21:57:51 +0800

    ssss

---
+++ promise.js
@@ -0,0 +1,26 @@
+const RESOLVE ='resolved';
+const REJECT = "rejected";
+const PENGING ="pending";
+class  JJPromise {
+      status= PENGING;
+      result = undefined;
+      reason = undefined;
+  constructor (excution){
+    
+      const resolve = (result)=>{
+       if(this.status ===PENGING){
+          this.result = result;
+          this.status = RESOLVE;
+       } 
+      }
+      const reject = (reason)=>{
+        if(this.status ===PENGING){
+           this.reason = reason;
+           this.status = REJECT;
+        } 
+       }
+      excution(resolve,reject)
+
+
+    }
+}
